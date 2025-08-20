7247e7c49df631faf511af804cf073cf654b848c
warriorOfEmptiness <phDiscordAndreyka@yandex.ru>
2025-08-20 04:17:07 +0300

    delete __pycache__ from github

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from goods.models import Products
 
 # Create your views here.
 
 
 def catalog(request):
+    
+    goods = Products.objects.all()
+
     context = {
         "title": "HOME - Каталог",
+        "goods":goods,
     }
     return render(request, "goods/catalog.html",context)
 
