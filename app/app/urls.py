2ef7faa80c3f8c92068ff179f718f258042a552a
warriorOfEmptiness <phDiscordAndreyka@yandex.ru>
2025-08-20 15:27:26 +0300

    add simle_tag

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include 
-from app.settings import DEBUG
+from app import settings
+from django.conf.urls.static import static
 
 
 
@@ -11,7 +12,10 @@ urlpatterns = [
 
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/",include("debug_toolbar.urls")),
+
     ]
+
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
